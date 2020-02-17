a2568bbfc399896311750e211f08978c6a3bc4bc
Andrew <48319785+isAloneWanderer@users.noreply.github.com>
2020-02-17 19:05:33 +0300

    fix getolidayis function

--- utils/get-holiday-list.js
+++ utils/get-holiday-list.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
+const monthOrder = [
+  'Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
+  'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'
+]
+
 module.exports = (month, day, ctx) => {
   const dataFilePath = path.join(__dirname, '..', 'data', 'holidays-list.json')
 
