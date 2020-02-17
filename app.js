a878a7a08f25d0fc77c28d62d001e06825745224
Andrew <48319785+isAloneWanderer@users.noreply.github.com>
2020-02-17 19:11:58 +0300

    add rest commands

--- app.js
+++ app.js
@@ -30,35 +30,33 @@ bot.command('/tomorrow', ctx => {
   const date = new Date()
   const month = date.getMonth()
   const day = date.getDate() + 1
-  
+
   getHolidayList(month + 1, day, ctx)
 })
 
+bot.hears(/\/date \d+ .+/, ctx => {
+  const [, day, monthName] = match.split(' ')
+
+  const month = monthOrder.findIndex(m => m.trim().toLowerCase() == monthName.trim().toLowerCase())
+
+  getHolidayList(month + 1, +day, ctx)
+})
+
+bot.on('text', ctx => {
+  const match = /\d .+/.match(ctx.message)
+  if (match) {
+    const [day, monthName] = match.split(' ')
+    const month = monthOrder.findIndex(m => m.trim().toLowerCase() == monthName.trim().toLowerCase())
+    getHolidayList(month + 1, +day, ctx)
+  } else {
+    ctx.reply('Неизвестная команда')
+  }
+})
+
+bot.hears(/\/date \d+ \d+/, ctx => {
+  const [, day, month] = ctx.message.split(' ')
+
+  getHolidayList(+day, +month, ctx)
+})
+
 bot.startPolling()
-
-// bot.command(/\/date \d+ .+/, ctx => {
-  //   const [, day, monthName] = match.split(' ')
-  
-//   const month = monthOrder.findIndex(m => m.trim().toLowerCase() == monthName.trim().toLowerCase())
-
-//   getHolidayList(month + 1, +day, ctx)
-// })
-
-// bot.on('message', ctx => {
-  //   const match = /\d .+/.match(ctx.message)
-//   if (match) {
-  //     const [day, monthName] = match.split(' ')
-//     const month = monthOrder.findIndex(m => m.trim().toLowerCase() == monthName.trim().toLowerCase())
-//     getHolidayList(month + 1, +day, ctx)
-//   } else {
-  //     ctx.reply('Неизвестная команда')
-  //   }
-  // })
-  
-  // bot.command(/\/date \d+ \d+/, ctx => {
-    //   const chatId = msg.chat.id
-    
-    //   const [, day, month] = match[0].split(' ')
-    
-    //   getHolidayList(+day, +month, chatId)
-    // })
